b6caca051d5a4171a139475e77f85a16e9efe6b6
tiaswidiatmika <tiaswidiatmika@gmail.com>
2021-02-21 18:25:46 +0800

    initial navbar

---
+++ resources/views/layouts/includes/navbar.blade.php
@@ -0,0 +1,21 @@
+<div class="h-16 fixed top-0 inset-x-0 shadow-sm bg-green-900 text-indigo-200">
+  <div class="container mx-auto flex justify-between items-center h-full">
+
+    <!-- {{-- left section --}} -->
+    <div  class="flex">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/7/75/Reports_Flat_Icon.svg" alt="" class="w-12">
+      <ul class="flex items-center space-x-4 ml-4">
+        <li class="relative group"><a class="group-hover:text-white" href="">All Reports</a>
+          <ul class="absolute mt-8 p-4 bg-gray-100 border border-gray-300 w-48 shadow-lg opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out transform -translate-y-6 group-hover:translate-y-0"></ul>
+        </li>
+        <li><a class="relative-group" href="">Create New</a></li>
+      </ul>
+    </div>
+
+    <!-- {{-- right section --}} -->
+    <div class="flex items-center p-4 space-x-4">
+      <a href="" class="text-indigo-200 p-1">Login</a>
+      <a href="" class="block pt-1 pb-1 pl-1.5 pr-1.5 bg-blue-500 rounded-sm">Sign Up</a>
+    </div>
+  </div>
+</div>
